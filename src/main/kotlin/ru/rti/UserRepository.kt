6aecb5f54ec88726fc01db25923bce25c03b08f7
xqnmqx <xqnmqx>
2018-11-08 08:10:17 +0300

    JPA repository transactional annotation;

--- src/main/kotlin/ru/rti/UserRepository.kt
+++ src/main/kotlin/ru/rti/UserRepository.kt
@@ -1,7 +1,5 @@
 package ru.rti
 
 import org.springframework.data.jpa.repository.JpaRepository
-import javax.transaction.Transactional
 
-@Transactional(Transactional.TxType.MANDATORY)
 internal interface UserRepository: JpaRepository<User, Long>
